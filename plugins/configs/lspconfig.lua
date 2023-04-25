a925dfbf2501d7f27d7a850fe04c5d8c99f3fc26
David Wu <david6906817@gmail.com>
2023-04-25 09:36:16 +0800

    Setup rust-analyzer.

--- plugins/configs/lspconfig.lua
+++ plugins/configs/lspconfig.lua
@@ -4,3 +4,8 @@ require("lspconfig").tsserver.setup {
   on_attach = default_config.on_attach,
   capabilities = default_config.capabilities,
 }
+
+require("lspconfig").rust_analyzer.setup {
+  on_attach = default_config.on_attach,
+  capabilities = default_config.capabilities,
+}
