4350e53ceb7a2f8cf204d4e31b28a6e4249c395a
David Wu <david6906817@gmail.com>
2023-08-27 01:26:11 +0800

    Add arduino-language-server.

--- plugins/configs/lspconfig.lua
+++ plugins/configs/lspconfig.lua
@@ -2,15 +2,24 @@ local default_config = require("plugins.configs.lspconfig")
 
 require("lspconfig").tsserver.setup {
   on_attach = default_config.on_attach,
-  capabilities = default_config.capabilities,
+  -- capabilities = default_config.capabilities,
 }
 
 require("lspconfig").rust_analyzer.setup {
   on_attach = default_config.on_attach,
-  capabilities = default_config.capabilities,
+  -- capabilities = default_config.capabilities,
 }
 
 require("lspconfig").clangd.setup {
   on_attach = default_config.on_attach,
-  capabilities = default_config.capabilities,
+  -- capabilities = default_config.capabilities,
+}
+
+require("lspconfig").arduino_language_server.setup {
+  on_attach = default_config.on_attach,
+  cmd = {
+    'arduino-language-server',
+    '-cli-config', '~/arduino-cli.yaml',
+    '-fqbn', 'arduino:avr:nano'
+  }
 }
