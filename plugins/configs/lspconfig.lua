359e61bb55158ee09bd39940b6dda236befc721c
David Wu <davidwu@dgm-tw.com>
2023-11-30 14:23:32 +0800

    Enable exrc option. Introduce global custom_is_arduino_project variable for deciding setting up arduino project or clang project.

--- plugins/configs/lspconfig.lua
+++ plugins/configs/lspconfig.lua
@@ -10,24 +10,27 @@ require("lspconfig").rust_analyzer.setup {
   -- capabilities = default_config.capabilities,
 }
 
-require("lspconfig").clangd.setup {
-  on_attach = default_config.on_attach,
-  -- capabilities = default_config.capabilities,
-  cmd = {
-    'clangd',
-    '-query-driver', '/usr/bin/arm-none-eabi-g*'
+if vim.g.custom_is_arduino_project
+then
+  require("lspconfig").arduino_language_server.setup {
+    on_attach = default_config.on_attach,
+    cmd = {
+      'arduino-language-server',
+      '-cli-config', '~/arduino-cli.yaml',
+      '-fqbn', 'rp2040:rp2040:rpipico'
+    },
+    filetypes = {
+      'arduino',
+      'cpp'
+    }
   }
-}
-
-require("lspconfig").arduino_language_server.setup {
-  on_attach = default_config.on_attach,
-  cmd = {
-    'arduino-language-server',
-    '-cli-config', '~/arduino-cli.yaml',
-    '-fqbn', 'rp2040:rp2040:rpipico'
-  },
-  filetypes = {
-    'arduino',
-    'cpp'
+else
+  require("lspconfig").clangd.setup {
+    on_attach = default_config.on_attach,
+    -- capabilities = default_config.capabilities,
+    cmd = {
+      'clangd',
+      '-query-driver', '/usr/bin/arm-none-eabi-g*'
+    }
   }
-}
+end
